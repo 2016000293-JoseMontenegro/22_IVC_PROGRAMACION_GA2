c19027e36aacee32fb28f086efabc3678b010b3d
2016000293-JoseMontenegro <j.montenegro5adb@gmail.com>
2026-04-07 10:32:19 -0600

    Add files via upload

---
+++ DiaDeLaSemana.cs
@@ -0,0 +1,41 @@
+﻿//Un programa que evalue un dia en número 
+//y muestre el dia en letras segun el numero
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("DIAS DE LA SEMANA");
+        //Variable
+        int dia;
+        //Solicitar datos
+        dia = Convert.ToInt32(Console.ReadLine());
+
+        switch (dia)
+        {
+            case 1:
+                Console.WriteLine("Lunes");
+                break;
+            case 2:
+                Console.WriteLine("Martes");
+                break;
+            case 3:
+                Console.WriteLine("Miercoles");
+                break;
+            case 4: 
+                Console.WriteLine("Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Viernes");
+                break;
+            case 6:
+                Console.WriteLine("Sabado");
+                break;
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+            default:
+                Console.WriteLine("DIGITE UN VALOR ENTRE 1 Y EL 7");
+                break;
+        }
+    }
+}
